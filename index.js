5a96521c679526ece15ec34cb53c7c9187eeaf2e
florence <loracodes3@gmail.com>
2024-10-15 11:29:02 +0300

    destructively update object

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const employee = {
 function updateEmployeeWithKeyAndValue(employee, key, value){
     return { ...employee, [key]: value };
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key] = value;
+        return employee;
+      
+}
 function deleteFromEmployeeByKey(employee, key){}
 function destructivelyDeleteFromEmployeeByKey(employee, key){}
