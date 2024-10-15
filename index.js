beaa8d0672cabaab938f5f626fb2131e7626ccec
florence <loracodes3@gmail.com>
2024-10-15 11:26:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name: "florence",
+    streetAddress: "11 Broadway"
+  };
+  
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){}
+function deleteFromEmployeeByKey(employee, key){}
+function destructivelyDeleteFromEmployeeByKey(employee, key){}
